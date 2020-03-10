fa7310eab86bb3054fc05e7ea74112d047563f79
Femi Opanuga <femathic@gmail.com>
2020-03-10 16:55:59 +0100

    Integrate Tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/tailwind.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="w-48" alt="logo" />
         <p>
           Edit
           <code>src/App.js</code>
